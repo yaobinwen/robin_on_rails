54c588c5d1387a08df502a7decee746a7541a98b
yaobinwen <robin.wyb@gmail.com>
2019-03-09 01:04:39 -0500

    Implement URL parsing and '/' response

--- security/oauth2/demo/client/client.py
+++ security/oauth2/demo/client/client.py
@@ -5,14 +5,46 @@
 
 
 import http.server
+import re
 
 
 class ClientRequestHandler(http.server.BaseHTTPRequestHandler):
 
+    def _parse_path(self):
+        m = re.match(
+            pattern=r'^(\/(?:[\w\.]+\/)*(?:[\w\.]+)?)(\?(?:(?:\w+)=(?:\w+)&?)+)?(#\w+)?$',
+            string=self.path,
+        )
+        path, query_str, frag = m.group(1, 2, 3)
+
+        queries = None
+        if query_str is not None:
+            query_parts = re.split(
+                # Skip '?' so we have a even number of equations.
+                pattern=r'[=&]', string=query_str[1:]
+            )
+            queries = {}
+            for i in range(0, len(query_parts), 2):
+                k = query_parts[i]
+                v = query_parts[i+1]
+                if k not in queries:
+                    queries[k] = v
+                else:
+                    v0 = queries[k]
+                    if not isinstance(v0, list):
+                        queries[k] = [v0, v]
+                    else:
+                        queries[k].append(v)
+
+        return path, queries, frag
+
     def do_GET(self):
+        path, queries, fragment = self._parse_path()
         self.send_response(code=200)
         self.send_header('Content-type','text/html')
         self.end_headers()
+        if path == '/':
+            self.wfile.write(bytes('<h1>Hello, client!</h1>', 'utf8'))
 
 
 def main():
