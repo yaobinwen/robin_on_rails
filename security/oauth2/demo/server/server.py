092fe977a4241202eabd621316e67f51b53490b7
yaobinwen <robin.wyb@gmail.com>
2019-03-16 21:19:37 -0400

    Implement server's authorization page

--- security/oauth2/demo/server/server.py
+++ security/oauth2/demo/server/server.py
@@ -2,15 +2,28 @@
 # https://docs.python.org/3/library/http.server.html
 
 
+import jinja2
+
 from shared.http_request_handler_helper import HTTPRequestHandlerHelper
+
+
+# The global Jinja2 Template ENVironment.
+T_ENV = jinja2.Environment(
+    loader=jinja2.PackageLoader('server', 'templates'),
+    autoescape=jinja2.select_autoescape(['html'])
+)
 
 
 class OAuthRequestHandler(HTTPRequestHandlerHelper):
 
     def do_GET(self):
+        path, queries, fragment = self._parse_path()
         self.send_response(code=200)
         self.send_header('Content-type','text/html')
         self.end_headers()
+        if path == '/oauth/authorize':
+            html = T_ENV.get_template('authorize.html').render()
+            self.wfile.write(bytes(html, 'utf8'))
 
 
 def get_publish_address():
