6a6eeb30538d9973ae0757a4592be318de93d2a9
Yaobin Wen <robin.wyb@gmail.com>
2020-03-16 20:52:22 +0000

    Add more code to test nlohmann::json behaviors

--- lang/c_cpp/code/programs/json.cpp
+++ lang/c_cpp/code/programs/json.cpp
@@ -5,11 +5,7 @@
 
 #include "../external/json.hpp"
 
-class JSONVerifier : public ::testing::Test
-{
-};
-
-TEST_F(JSONVerifier, test_initialization_with_different_types)
+TEST(nlohmann_json, test_initialization_with_different_types)
 {
     using ::nlohmann::json;
 
@@ -23,4 +19,29 @@ TEST_F(JSONVerifier, test_initialization_with_different_types)
 
     std::map<std::string, char> obj = { { "1", 'a' }, { "2", 'b' } };
     EXPECT_EQ(json(obj).dump(), "{\"1\":97,\"2\":98}");
+}
+
+TEST(nlohmann_json, test_modifiers)
+{
+    using ::nlohmann::json;
+
+    json j = json::parse("{\"a\":10, \"b\":20}");
+    j += json::object_t::value_type("c", 30);
+    EXPECT_EQ(j.dump(), "{\"a\":10,\"b\":20,\"c\":30}");
+}
+
+TEST(nlohmann_json, test_iterator)
+{
+    using ::nlohmann::json;
+
+    std::map<std::string, int> items = { { "a", 10 }, { "b", 20 } };
+
+    json j;
+    j += json::object_t::value_type("a", 10);
+    j += json::object_t::value_type("b", 20);
+
+    for (auto const &[key, value] : j.items())
+    {
+        EXPECT_EQ(items.at(key), value);
+    }
 }
