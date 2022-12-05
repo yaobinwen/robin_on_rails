5a43e74b3f0a3eaaf0b7441be18c32e50e8a733c
Yaobin Wen <robin.wyb@gmail.com>
2022-12-05 13:56:41 +0000

    Add more C++ vector demo code

--- C_Cpp/code/programs/cpp_demo/vector.cpp
+++ C_Cpp/code/programs/cpp_demo/vector.cpp
@@ -3,6 +3,49 @@
 
 #include <gtest/gtest.h>
 
+TEST(Test_vector, test_constructor)
+{
+    {
+        std::vector<int> empty_v;
+        EXPECT_TRUE(empty_v.empty());
+        EXPECT_TRUE(0U == empty_v.size());
+        EXPECT_TRUE(0U == empty_v.capacity());
+    }
+
+    {
+        std::vector<int> v(3U, 99);
+        EXPECT_EQ(3U, v.size());
+
+        for (size_t i = 0; i < v.size(); ++i)
+        {
+            EXPECT_EQ(99, v.at(i));
+        }
+    }
+
+    {
+        constexpr int default_int = int();
+        std::vector<int> v(4U);
+        EXPECT_EQ(4U, v.size());
+
+        for (size_t i = 0; i < v.size(); ++i)
+        {
+            EXPECT_EQ(default_int, v.at(i));
+        }
+    }
+
+    {
+        std::vector<int> v1{1, 3, 5};
+        std::vector<int> v2(v1.begin(), v1.end());
+
+        EXPECT_EQ(v1.size(), v2.size());
+
+        for (size_t i = 0; i < v1.size(); ++i)
+        {
+            EXPECT_EQ(v1.at(i), v2.at(i));
+        }
+    }
+}
+
 TEST(Test_vector, test_front)
 {
     std::vector<int> empty_v;
