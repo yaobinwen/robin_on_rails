2669a47213983879361e66aa7634a0897cf1695e
Yaobin Wen <robin.wyb@gmail.com>
2023-08-10 23:14:09 +0000

    Python: Add code to demo `datetime`

--- Python/datetime/datetime_demo.py
+++ Python/datetime/datetime_demo.py
@@ -8,7 +8,69 @@ This document is referred to as `DOC` in the code.
 
 import unittest
 
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
+
+
+class Test_datetime(unittest.TestCase):
+    def test_today(self):
+        """Demo of `datetime.datetime.today()`.
+
+        https://docs.python.org/3/library/datetime.html#datetime.datetime.today
+
+        This method returns the local time but WITHOUT time zone info.
+        Therefore, simply looking at the returned object by `today()` one would
+        not be able to tell if it's UTC or local time.
+
+        Therefore, if we assume that one computer is always in one time zone,
+        `today()` is the best to be used on the same local computer without
+        passing it anywhere else, not even on another local computer because
+        the other computer may be in a different time zone.
+        """
+        today = datetime.today()
+        print(f"today: {today} (local time, NO time zone info)")
+        self.assertTrue(today.year > 0)
+        self.assertTrue(today.month > 0)
+        self.assertTrue(today.day > 0)
+        self.assertTrue(today.hour >= 0)
+        self.assertTrue(today.minute >= 0)
+        self.assertTrue(today.second >= 0)
+        self.assertTrue(today.microsecond >= 0)
+
+        # The `today` object doesn't have any time zone info, making the user
+        # unable to tell if it is a UTC time or a local time.
+        self.assertIsNone(today.tzinfo)
+
+    def test_now(self):
+        """Demo of `datetime.datetime.now()`.
+
+        https://docs.python.org/3/library/datetime.html#datetime.datetime.now
+        """
+        now_no_tz = datetime.now()
+
+        tz_est = timezone(offset=timedelta(hours=-4), name="EST")
+        now_with_tz = datetime.now(tz=tz_est)
+
+        now_in_utc = datetime.now(tz=timezone.utc)
+
+        print(f"now without tz: {now_no_tz}")
+        print(f"   now with tz: {now_with_tz}")
+        print(f"    now in UTC: {now_in_utc}")
+
+    def test_utcnow(self):
+        """Demo of `datetime.datetime.utcnow()`.
+
+        https://docs.python.org/3/library/datetime.html#datetime.datetime.utcnow
+
+        `utcnow()` has the same issue as `today()` that it doesn't contain the
+        time zone info, so by looking at the `utcnow` object, one can't tell if
+        it is a UTC time or some local time. So `now(tz=timezone.utc)` is
+        preferred.
+        """
+        utcnow = datetime.utcnow()
+        now_in_utc = datetime.now(tz=timezone.utc)
+
+        print(f"    utcnow: {utcnow}")
+        print(f"now_in_utc: {now_in_utc}")
 
 
 class Test_timedelta(unittest.TestCase):
