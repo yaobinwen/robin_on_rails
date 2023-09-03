b68dba56c9bd222631701a615462ec08621faa61
Yaobin Wen <robin.wyb@gmail.com>
2023-09-03 22:29:29 +0000

    Assembly: generate debug info when compiling assembly code

--- Assembly/asmtutor/build.sh
+++ Assembly/asmtutor/build.sh
@@ -5,5 +5,9 @@ ASM_FILENAME="$1"
 
 FILENAME_STEM="$(python3 -c "from pathlib import Path; p=Path('${ASM_FILENAME}'); print(p.stem)")"
 
-nasm -f elf -o "${FILENAME_STEM}.o" "$ASM_FILENAME"
+# `-g` generates the debug symbols.
+# `-F dwarf` generates the debug symbols in DWARF format.
+nasm \
+  -g -F dwarf \
+  -f elf -o "${FILENAME_STEM}.o" "$ASM_FILENAME"
 ld -m elf_i386 "${FILENAME_STEM}.o" -o "${FILENAME_STEM}.exe"
