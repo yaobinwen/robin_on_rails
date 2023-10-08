397aafe42b38972a3a9975c527c78f7be8cb9f47
Yaobin Wen <robin.wyb@gmail.com>
2023-10-08 19:01:23 +0000

    Assembly: Add more code

---
+++ Assembly/asmtutor/args.asm
@@ -0,0 +1,20 @@
+%include  "func.asm"
+ 
+SECTION .text
+global  _start
+ 
+_start:
+
+    ; First value on the stack is the number of arguments
+    pop     ecx
+ 
+nextArg:
+    cmp     ecx, 0h         ; check to see if we have any arguments left
+    jz      noMoreArgs      ; if zero flag is set jump to noMoreArgs label (jumping over the end of the loop)
+    pop     eax             ; pop the next argument off the stack
+    call    sprintLF        ; call our print with linefeed function
+    dec     ecx             ; decrease ecx (number of arguments left) by 1
+    jmp     nextArg         ; jump to nextArg label
+ 
+noMoreArgs:
+    call    quit
