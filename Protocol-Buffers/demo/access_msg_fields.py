b9c899e1d55481d4e9e0e899a10b5f3d0e7f49d5
Yaobin Wen <robin.wyb@gmail.com>
2024-05-14 15:34:28 -0400

    Protocol Buffers: Update demo code

--- Protocol-Buffers/demo/access_msg_fields.py
+++ Protocol-Buffers/demo/access_msg_fields.py
@@ -2,6 +2,7 @@ import unittest
 
 from protobufs.v0.building_pb2 import Building
 from protobufs.v0.maps_pb2 import Management
+from protobufs.v0.school_pb2 import Class, Student, Teacher
 
 
 class Test_access_simple_fields(unittest.TestCase):
@@ -30,6 +31,65 @@ class Test_access_simple_fields(unittest.TestCase):
         self.assertEqual(self.b1.years_since_built, 11)
 
 
+# API doc: https://googleapis.dev/python/protobuf/latest/google/protobuf/internal/containers.html#google.protobuf.internal.containers.RepeatedCompositeFieldContainer
+class Test_access_repeated_field(unittest.TestCase):
+    """Demo how to access a message's `repeated` field.
+    """
+
+    def setUp(self):
+        self.class9604 = Class(
+            id="9604",
+            head_teacher=Teacher(name="John Doe"),
+            students=[
+                Student(name="Alice"),
+                Student(name="Bob"),
+                Student(name="Carrie"),
+                Student(name="David"),
+            ],
+        )
+
+    def test_length(self):
+        self.assertEqual(len(self.class9604.students), 4)
+
+    def test_type(self):
+        students_type = self.class9604.students.__class__.__name__
+        self.assertEqual(students_type, "RepeatedCompositeContainer")
+
+        student_list = list(self.class9604.students)
+        self.assertIsInstance(student_list, list)
+
+    def test_subscription(self):
+        alice = self.class9604.students[0]
+        self.assertEqual(alice.name, "Alice")
+
+        david = self.class9604.students[3]
+        self.assertEqual(david.name, "David")
+
+    def test_iterate(self):
+        for s in self.class9604.students:
+            self.assertIn(s.name, ["Alice", "Bob", "Carrie", "David"])
+
+    def test_convert_to_set(self):
+        s = set([s.name for s in self.class9604.students])
+        self.assertSetEqual(s, set(["Alice", "Bob", "Carrie", "David"]))
+
+    def test_add_elements(self):
+        students = self.class9604.students
+        emily = students.add(name="Emily")
+        self.assertEqual(len(self.class9604.students), 5)
+        self.assertEqual(emily.name, "Emily")
+
+    def test_extend_elements(self):
+        students = self.class9604.students
+        students.extend([Student(name="Emily"), Student(name="Frank")])
+        self.assertEqual(len(self.class9604.students), 6)
+
+        emily = self.class9604.students[4]
+        self.assertEqual(emily.name, "Emily")
+        frank = self.class9604.students[5]
+        self.assertEqual(frank.name, "Frank")
+
+
 class Test_access_oneof_field(unittest.TestCase):
     """Demo how to access a message's `oneof` field.
     """
@@ -51,16 +111,23 @@ class Test_access_map_fields(unittest.TestCase):
         self.managers = {"Ada": self.b1, "Bob": self.b2}
         self.management = Management(managers=self.managers)
 
-    def test_read_map_entries_from_keys(self):
+    def test_check_key_existence(self):
         m = self.management.managers
 
         # `keys()` returns a `KeyView` that can be converted into a set.
         manager_names = set(m.keys())
         self.assertSetEqual(set(self.managers.keys()), manager_names)
 
-        # Check existence.
-        self.assertIn("Ada", manager_names)
-        self.assertIn("Bob", manager_names)
+        # Check existence directly.
+        self.assertIn("Ada", m)
+        self.assertIn("Bob", m)
+
+        # Check existence using `keys()`.
+        self.assertIn("Ada", m.keys())
+        self.assertIn("Bob", m.keys())
+
+    def test_read_map_entries_from_keys(self):
+        m = self.management.managers
 
         # Iterate through the keys.
         for manager_name in m.keys():
--- Protocol-Buffers/demo/enums.py
+++ Protocol-Buffers/demo/enums.py
@@ -1,7 +1,7 @@
 import unittest
 
 from google.protobuf import json_format
-from protobufs.v0.address_pb2 import STATE_NY, State
+from protobufs.v0.address_pb2 import STATE_UNSPECIFIED, STATE_NY, State
 
 
 class Test_enum(unittest.TestCase):
@@ -21,7 +21,75 @@ class Test_enum(unittest.TestCase):
         # so one can't access it as `State.STATE_NY`.
         self.assertFalse(hasattr(State, "STATE_NY"))
 
+        # The concrete enum values such as `STATE_NY` is not owned by the enum
+        # itself `State`, so accessing `State.STATE_NY` will raise an error.
         self.assertRaises(AttributeError, lambda: State.STATE_NY)
+
+    def test_enum_enumerate_items(self):
+        """Demo how to enumerate all the possible values of an enum.
+        """
+        # Call `items()` to enumerate the enum's values and corresponding names.
+        # Example output:
+        # [('STATE_UNSPECIFIED', 0), ('STATE_AL', 1), ('STATE_AK', 2), ...]
+        items = State.items()
+        self.assertIsInstance(items, list)
+
+        for item in items:
+            # Each item is a 2-element tuple.
+            self.assertIsInstance(item, tuple)
+            self.assertEqual(len(item), 2)
+            # The first element is a string that represents the name of the
+            # enum value.
+            self.assertIsInstance(item[0], str)
+            # The second element is an integer that represents the numeric value
+            # of the enum value.
+            self.assertIsInstance(item[1], int)
+
+    def test_enum_enumerate_keys(self):
+        """Demo how to enumerate all the keys of an enum.
+        """
+        # The keys are the names of the enum values, such as the string
+        # "STATE_AL", "STATE_NY", etc.
+        keys = State.keys()
+        self.assertIsInstance(keys, list)
+
+        for key in keys:
+            # Each key is a string that represents the name of the enum value.
+            self.assertIsInstance(key, str)
+
+    def test_enum_enumerate_values(self):
+        """Demo how to enumerate all the values of an enum.
+        """
+        # The values are the numeric values of the enum values, such as 1 for
+        # STATE_AL, 32 for STATE_NY, etc.
+        values = State.values()
+        self.assertIsInstance(values, list)
+
+        for value in values:
+            # Each value is a numeric value for that enum value.
+            self.assertIsInstance(value, int)
+
+    def test_get_str_representation_of_enum(self):
+        """Demo how to get the string representation of an enum value given its
+        numeric value.
+        """
+        # Call the `Name` method to get the string representation.
+        self.assertEqual(State.Name(0), "STATE_UNSPECIFIED")
+        self.assertEqual(State.Name(STATE_NY), "STATE_NY")
+
+        # Out-of-range values will cause `ValueError`:
+        self.assertRaises(ValueError, State.Name, -9999)
+
+    def test_get_numeric_representation_of_enum(self):
+        """Demo how to get the numeric value of an enum value given given its
+        string representation.
+        """
+        # Call the `Value` method to get the numeric values.
+        self.assertEqual(State.Value("STATE_UNSPECIFIED"), STATE_UNSPECIFIED)
+        self.assertEqual(State.Value("STATE_NY"), STATE_NY)
+
+        # Non-recognizable strings will cause `ValueError`.
+        self.assertRaises(ValueError, State.Value, "STATE_IMAGINATION")
 
 
 if __name__ == "__main__":
