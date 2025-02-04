f64aa8f893e057f6d9bf43de6efaf97a7b6be73e
Yaobin Wen <robin.wyb@gmail.com>
2025-02-04 17:55:17 -0500

    `Python`: Move stuff to the repo `yaobinwen/learn-cpython`

--- Python/mock/C.py
+++
@@ -1,9 +0,0 @@
-"""This module implements classes and functions for patch demo.
-"""
-
-
-ORIGINAL_HI_MSG = "Hi~ This is C."
-
-
-def say_hi():
-    return ORIGINAL_HI_MSG
--- Python/mock/run_demo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python3 mock_demo.py
-python3 patch_demo.py
